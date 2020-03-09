e6dd8bf94e4223021f6bf3baa1a558a5bc458597
Christopher Lamm <chris@theantichris.com>
2020-03-09 10:29:13 -0400

    added health check

--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"io"
+	"log"
+	"net/http"
 
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("/health", Health).Methods("GET")
+
+	log.Fatal(http.ListenAndServe(":8000", router))
+}
+
+// Health handles the GET request to /health.
+func Health(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+
+	io.WriteString(w, `{"alive": true}`)
 }
