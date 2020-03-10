02c58ea88614d6c19ba08a2e446613da1e7f4360
Christopher Lamm <chris@theantichris.com>
2020-03-10 10:33:29 -0400

    implemented graceful server shutdown

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
 
 	"github.com/gorilla/mux"
 	"github.com/theantichris/todo-api/handlers"
@@ -11,16 +15,40 @@ import (
 
 // TODO: add to env
 var port = ":8000"
-var session, _ = mgo.Dial("127.0.0.1")
-var c = session.DB("TodoDB").C("Todo")
+var db, _ = mgo.Dial("127.0.0.1")
+var c = db.DB("TodoDB").C("Todo")
 
 func main() {
-	session.SetMode(mgo.Monotonic, true)
-	defer session.Close()
+	db.SetMode(mgo.Monotonic, true)
+	defer db.Close()
 
 	router := mux.NewRouter()
 	router.HandleFunc("/health", handlers.HealthCheckHandler).Methods("GET")
 
-	// TODO: gracefully exit on interrupt
-	log.Fatal(http.ListenAndServe(port, router))
+	server := &http.Server{
+		Addr:         "0.0.0.0" + port,
+		WriteTimeout: time.Second * 15,
+		ReadTimeout:  time.Second * 15,
+		IdleTimeout:  time.Second * 60,
+		Handler:      router,
+	}
+
+	go func() {
+		if err := server.ListenAndServe(); err != nil {
+			log.Println(err)
+		}
+	}()
+
+	interruptChan := make(chan os.Signal, 1)
+	signal.Notify(interruptChan, os.Interrupt)
+
+	<-interruptChan
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*15)
+	defer cancel()
+
+	server.Shutdown(ctx)
+
+	log.Println("shutting down")
+	os.Exit(0)
 }
